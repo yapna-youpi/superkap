bdece0fe94c41038bdf8fb381a86f2cea339a157
yapna-youpi <yapnayoupi@gmail.com>
2021-12-12 12:57:14 +0100

    update navbar responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,18 +18,18 @@ function Header() {
 
                     <nav id="navbar" className="navbar">
                         <ul>
-                        <li><Link className="nav-link scrollto active" to="/">Home</Link></li>
-                        <li><Link className="nav-link scrollto" to="/Business">Business</Link></li>
-                        <li><Link className="nav-link scrollto" to="/Crypto">Superkap Crypto</Link></li>
-                        <li><Link className="nav-link scrollto " to="/Immo">Immobilier</Link></li>
-                        <li><Link className="nav-link scrollto" to="/Academy">Academy</Link></li>
-                        <li className="dropdown"><a href="#"><span style={{textDecoration: 0}}>User</span> <i className="bi bi-chevron-down"></i></a>
-                            <ul className='text-center '>
-                                <Link to="/Login" className='="list'><li>Login Superkap</li></Link>
-                                <Link to="/SignUp" className='="list'><li><span>SingUp</span></li></Link>
-                            </ul>
-                        </li>
-                        <li><a className="nav-link scrollto" href="#contact">Support</a></li>
+                            <li><Link className="nav-link scrollto active" to="/">Home</Link></li>
+                            <li><Link className="nav-link scrollto" to="/Business">Business</Link></li>
+                            <li><Link className="nav-link scrollto" to="/Crypto">Superkap Crypto</Link></li>
+                            <li><Link className="nav-link scrollto " to="/Immo">Immobilier</Link></li>
+                            <li><Link className="nav-link scrollto" to="/Academy">Academy</Link></li>
+                            <li className="dropdown"><a href="#"><span style={{textDecoration: 0}}>User</span> <i className="bi bi-chevron-down"></i></a>
+                                <ul className='text-center '>
+                                    <Link to="/Login" className='="list'><li>Login Superkap</li></Link>
+                                    <Link to="/SignUp" className='="list'><li><span>SingUp</span></li></Link>
+                                </ul>
+                            </li>
+                            <li><a className="nav-link scrollto" href="#contact">Support</a></li>
                         </ul>
                         <i className="bi bi-list mobile-nav-toggle"></i>
                     </nav>
