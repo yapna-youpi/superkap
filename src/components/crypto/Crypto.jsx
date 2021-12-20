03d77acd3d41457bfad2a7750d90b537b1060585
yapna-youpi <yapnayoupi@gmail.com>
2021-12-20 22:13:19 +0100

    fin de la page crypto lundi 20

--- src/components/crypto/Crypto.jsx
+++ src/components/crypto/Crypto.jsx
@@ -6,9 +6,8 @@ import './crypto.css'
 import img1 from './assets/crypt5.jpg'
 import img2 from './assets/crypt2.jpg'
 import img3 from './assets/crypt3.jpg'
-import tof from './assets/tof1.jpeg'
-import logo from './assets/logo.png'
-import care from './assets/care.png'
+import tof3 from './assets/tof3.jpg'
+import tof from './assets/crypt4.jpg'
 import carte from './assets/carte.png'
 import crp1 from './assets/images crypto/dogde.jpeg'
 import crp2 from './assets/images crypto/tether.jpg'
@@ -84,8 +83,8 @@ function Crypto() {
                     <img className='partner1' src={crp1} alt='crypto' />
                 </div>
             </div>
-            <div className="trade">
-                <div className="trade-left">
+            <div className="trade py-4 ">
+                <div className="trade-left pt-3 p-4 p-md-0">
                     <h1>Nos services sur Blockchain</h1>
                     <p>Avec notre nouveau programme d'affiliation, vous aurez<br/> les outils les plus avancés du marché qui<br/>vous permettra de promouvoir Pursa et de générer des commissions avec nous !</p>
                     <dl>
@@ -95,49 +94,55 @@ function Crypto() {
                         <dt>HOLDING.</dt>
                         <dd>N’achetez pas aveuglement toute crypto, faites confiance aux professionnels pour<br/>
                              vous aider à investir sur les projets les plus rentables. Il est exclusivement réservé aux abonnés actifs.<br/>
-                            <button className="btn-crypto">En savoir plus</button> 
+                            <button onClick={()=>history.push('/Help')} className="btn-crypto py-2 my-2">En savoir plus</button> 
                         </dd>
 
                         <dt>MONTH TRADING</dt>
                         <dd>Ce service est destiné à tous ceux qui veulent gagner hebdomadairement et mensuellement de l’argent avec la cryptomonnaie. Il est exclusivement réservé aux abonnés actifs.<br/>
                              Découvrez nos signaux d’achats et de vente et profitez pour faire le plein de cryptos<br/>
-                            <button className="btn-crypto">En savoir plus</button> 
+                            <button onClick={()=>history.push('/Help')} className="btn-crypto py-2 my-2">En savoir plus</button> 
                         </dd>
                     </dl>               
                 </div>
-                <div className="trade-right">
-                    <p><img style={{width:'100%'}} src={tof} alt='image crypto' /></p>
+                <div  >
+                    <img className="trade-right" src={tof3} alt='image crypto' />
                 </div>
             </div>
-            <div className="target-form">
-                <div className="target-left">
-                    <div className="target-right-head">
-                        <img src={care} alt="image du trader" />
+            <div className="container target">
+                <div className="row">
+                    <div className="col-11 mr-4 col-md-8">
+                        <div className="row my-3">
+                            <div className="col-12 target-img1 col-md-4"></div>
+                            <div className="col-12 col-md-8">
+                                <h5>Comment acheter des crypto-monnaies sur notre plate forme ?</h5>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi obcaecati saepe expedita!</p>
+                                <button onClick={()=>history.push('/Buy')} className="btn btn-lg btn-warning">ACHETER CRYPTO</button>
+                            </div>
+                        </div><hr/>
+                        <div className="row my-3">
+                            <div className="col-12 target-img2 col-md-4"></div>
+                            <div className="col-12 col-md-8">
+                                <h5>vous desirez revendre ou vendre votre crypto-monnaie !</h5>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi obcaecati saepe expedita!</p>
+                                <button onClick={()=>history.push('/Sell')} className="btn btn-lg btn-success">VENDRE CRYPTO</button>
+                            </div>
+                        </div><hr/>
                     </div>
-                    <div className="target-right-body">
-                        <div onClick={()=>history.push('/Buy')} className="btn btn-lg target-buy">
-                            <img className='img-logo' src={logo} alt='acheter' />
-                            <span>Acheter</span>    
-                        </div>  
-                        <div  onClick={()=>history.push('/Sell')} className="btn btn-lg target-sell">
-                            <img className='img-logo' src={logo} alt='vendre' />
-                            <span>Vendre</span>   
-                        </div>  
+                    <div className="col-11 text-sm-center target-bg my-4 col-md-4">
+                        <div class="card" style={{width:'400px'}}>
+                            <img class="card-img-top" src={tof} alt="Card image" />
+                            <div class="card-body">
+                                <h6 class="card-title">Court de la crypto 24/24 et 7jours.</h6>
+                                <p class="card-text">abonnez vous à notre FAQ </p>
+                                <a onClick={()=>history.push('/Help')} class="btn btn-crypto">En Savoir Plus</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="target-right">    
-                    <h2>Comment acheter ou vendre votre crypto ?</h2>
-                    <p>quel que soit le moyen de paiement utilisé. Désormais, vous pouvez échanger 
-                        votre bitcoin au Cameroun contre MasterCard, MTN Mobile Money, 
-                        Orange Money ou VISA de manière rapide, simple et sécurisée,
-                    </p>
-                    <p>il vous suffit juste d’émettre votre commande et vous 
-                        serez servis dans les meilleurs délais et aux meilleurs prix du marché
-                    </p>
-                </div>
+                
             </div>
             <div className="credit">
-                <div className="credit-left">
+                <div className="credit-left w-sm-75">
                     <div className="credit-card"><img src={carte} alt='carte credit' /></div>
                 </div>
                 <div className="credit-right">
