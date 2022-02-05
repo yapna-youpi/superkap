5cedaf7ee0265747e358b2ed940d2fe1bdb6c33d
Folley-05 <aristotepascaldjounda@gmail.com>
2022-02-05 17:07:23 +0100

    change forms

--- src/components/buy/Buy.jsx
+++ src/components/buy/Buy.jsx
@@ -3,14 +3,23 @@ import { useHistory } from 'react-router-dom'
 import ReactLoading from 'react-loading'
 
 import './buy.css'
+const PaymentWays={
+    p1: [{lab: 'Carte Visa UBA cameroun', val: 'CB'}, {lab: 'MTN Mobile money', val: 'Mtn'}, {lab: 'Orange Money Cameroun', val: 'OM'}, {lab: 'MTN MOMO FLOAT CAMEROUN', val: 'MTNF'}, {lab: 'Carte Visa Crypto SuperKap', val: 'CBSK'}],
+    p2: [{lab: 'MOMO international', val: 'MOMO international'}],
+    p3: [{lab: 'Express Union', val: 'EU'}, {lab: 'Money gram', val: 'MG'}, {lab: 'Western Union', val: 'WU'}],
+    p4: [{lab: 'Cinet Pay', val: 'Cinet Pay '}],
+    p5: [{lab: 'Cinet Pay', val: 'Cinet Pay'}],
+    p6: [{lab: 'Money gram', val: 'MG'}, {lab: 'Werstern Union', val: 'WU'}, {lab: 'World Remit', val: 'WR'}, {lab: 'MOMO/OM', val: 'MOMO/OM'}],
+
+}
 
 function Buy() {
     const history =  useHistory();
 
     const [loader,setLoader] = useState(false);
 
-    const [state, setState]=useState({amount: "", fees: "", crypto: "Bitcoin", payment: "CB", wallet: "", numAccount: "", names: "", phone: ""})
-    const [transaction, setTransaction]=useState(true)
+    const [state, setState]=useState({amount: "", fees: "", crypto: "Bitcoin", country: 'CAMEROUN-p1', payment: "CB", wallet: "", numAccount: "", names: "", phone: ""})
+    const [transaction, setTransaction]=useState(false)
 
     const handleChange=(target)=>{
         setState({...state, [target.name]: target.value})
@@ -28,6 +37,7 @@ function Buy() {
                 "montant": state.amount,
                 "frais": state.amount*0.1,
                 "crypto": state.crypto,
+                "country": state.country.split('-')[0],
                 "mode_paiement": state.payment,
                 "numAccount": state.numAccount,
                 "adresse_crypto": state.wallet,
@@ -47,8 +57,13 @@ function Buy() {
         })
         return false
     }
-
+    const setPaymentsWays=()=>{
+        let way=state.country.split('-')[1]
+        console.log("the way ", way)
+        return PaymentWays[way].map((way, i)=><option key={`way-${i}`} value={way.val}>{way.lab}</option>)
+    }
     console.log(state)
+    // console.log(setPaymentsWays())
     return (
         <div className='buy container-fluid'>
             <h1 className='text-center mt-5 pt-5' style={{color:'white'}}>Acheter de la crypto</h1>
@@ -68,42 +83,51 @@ function Buy() {
                     <div className="main-buy">
                         <div className="agileits-top">
                                 <div className="form-group my-4">
-                                    <label className='label' for="select">Mode de paiement</label>
-                                    <select class="form-select" name="payment" aria-label="Default select example" onChange={(e)=>handleChange(e.target)}>
-                                        <option value="CB" selected>Carte Visa UBA cameroun</option>
-                                        <option value="Mtn">Mtn Mobile Money</option>
-                                        <option value="OM">Orange Money Cameroun</option>
-                                        <option value="MTNF">MTN MOMO FLOAT CAMEROUN</option>
-                                        <option value="CBSK">Carte Visa Crypto SuperKap</option>  
+                                    <label className='label' htmlFor="select">Choix de la crypto</label>
+                                    <select className="form-select" name="crypto" aria-label="Default select example" onChange={(e)=>handleChange(e.target)}>
+                                        <option value="Bitcoin" >Bitcoin</option>
+                                        <option value="Usdt">USDT (TRC20)</option>
+                                        <option value="Euro PAYEER">Euro payeer</option>
+                                        <option value="Litecoin">Litecoin </option>
+                                        <option value="Solana">Solana </option>
+                                        <option value="Autre">Autre... Contacter support </option>
                                     </select>
                                 </div>
                                 <div className="form-group my-4">
-                                    <label className='label' for="select">Choix de la crypto</label>
-                                    <select class="form-select" name="crypto" aria-label="Default select example" onChange={(e)=>handleChange(e.target)}>
-                                        <option value="Bitcoin" selected>Bitcoin</option>
-                                        <option value="Usdt">USDT (TRC20)</option>
-                                        <option value="Ep">Euro payeer</option>
-                                        <option value="Theter">Theter </option>
+                                    <label className='label' htmlFor="select">Choix du pays</label>
+                                    <select className="form-select" name="country" aria-label="Default select example" onChange={(e)=>handleChange(e.target)}>
+                                        <option value="CAMEROUN-p1" >CAMEROUN</option>
+                                        <option value="GABON/CONGO-p2">GABON/CONGO brazza </option>
+                                        <option value="TCHAD/CENTRAFRIQUE-p3">TCHAD/CENTRAFRIQUE</option>
+                                        <option value="RDC-p4">RD CONGO</option>
+                                        <option value="UMAO-p5">CTE D'IVOIRE/MALI/SENEGAL/BENIN/BURKINA FASO/GUINE CONAKRY</option>
+                                        <option value="Autres-p6">Autre Pays</option>
                                     </select>
                                 </div>
-                                <div class="form-group mb-3">
-                                    <label class="label" for="name">J'ai besoin de: </label>
-                                    <input type="number" name="amount" class="form-control input-buy" placeholder="montant crypto en $" required onChange={(e)=>handleChange(e.target)} />
+                                <div className="form-group my-4">
+                                    <label className='label' htmlFor="select">Mode de paiement</label>
+                                    <select className="form-select" name="payment" aria-label="Default select example" onChange={(e)=>handleChange(e.target)}>
+                                        {setPaymentsWays()}
+                                    </select>
                                 </div>
-                                <div class="form-group mb-3">
-                                    <label class="label" for="name">Adresse wallet</label>
-                                    <input type="text"  name="wallet" class="form-control input-buy" placeholder="Entrez l'adresse de votre wallet" required onChange={(e)=>handleChange(e.target)}/>
+                                <div className="form-group mb-3">
+                                    <label className="label" htmlFor="name">J'ai besoin de: </label>
+                                    <input type="number" name="amount" className="form-control input-buy" placeholder="montant crypto en $" required onChange={(e)=>handleChange(e.target)} />
+                                </div>
+                                <div className="form-group mb-3">
+                                    <label className="label" htmlFor="name">Adresse wallet</label>
+                                    <input type="text"  name="wallet" className="form-control input-buy" placeholder="Entrez l'adresse de votre wallet" required onChange={(e)=>handleChange(e.target)}/>
                                 </div>
                                 {
                                     (state.payment.indexOf("CB")+1) ? (
-                                        <div class="form-group mb-3">
-                                            <label class="label" for="name">numero carte</label>
-                                            <input type="text"  name="numAccount" class="form-control input-buy" placeholder="numero de la carte bancaire"
+                                        <div className="form-group mb-3">
+                                            <label className="label" htmlFor="name">numero carte</label>
+                                            <input type="text"  name="numAccount" className="form-control input-buy" placeholder="numero de la carte bancaire"
                                                 required onChange={(e)=>handleChange(e.target)}/>
                                         </div>):(
-                                <div class="form-group mb-3">
-                                    <label class="label" for="name">numero de compte</label>
-                                    <input type="text"  name="numAccount" class="form-control input-buy" placeholder="numero telephone du compte"
+                                <div className="form-group mb-3">
+                                    <label className="label" htmlFor="name">numero de compte</label>
+                                    <input type="text"  name="numAccount" className="form-control input-buy" placeholder="numero telephone du compte"
                                     required onChange={(e)=>handleChange(e.target)}/>
                                 </div>)
                                 }
@@ -113,53 +137,37 @@ function Buy() {
                 <div className=" col-md-12 col-lg-6 main-w3layouts wrapper">
                     <div className="main-buy">
                         <div className="agileits-top">
-                            {/* <form action="#" method="post" className='form-group' onSubmit={(e)=>handleSubmit(e)} > */}
-                                {/* <div class="form-group mb-3">
-                                    <label class="label" for="name">J'ai besoin de </label>
-                                    <input type="number" name="amount" class="form-control input-buy" placeholder="montant correspondant a la quandite de btc" required onChange={(e)=>handleChange(e.target)} />
-                                </div> */}
-                                {/* <div className="row">
-                                    <div className="col-6">Montant net en CFA</div>
-                                    <div className="col-6">Montant net en devise crypto</div>
-                                </div>
-                                <div className="input-group">
-                                    <input type="text" className="form-control" placeholder='montant net' required/>
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text">-</span>
-                                    </div> 
-                                    <input type="text" className="form-control" placeholder='devise net' required/>
-                                </div> */}
-                                <div class="form-group mb-3">
-                                    <label class="label" for="name">Montant a envoyer en XAF : </label>
-                                    <input type="tel" name="phone" class="form-control input-buy" placeholder="Telephone"
+                                <div className="form-group mb-3">
+                                    <label className="label" htmlFor="name">Montant a envoyer en XAF : </label>
+                                    <input type="tel" name="phone" className="form-control input-buy" placeholder="Telephone"
                                         disabled value={Math.floor(state.amount*1.1*550)} />
                                 </div>
-                                <div class="form-group mb-3">
-                                    <label class="label" for="name">frais en XAF : </label>
-                                    <input type="tel" name="phone" class="form-control input-buy" placeholder="Telephone"
+                                <div className="form-group mb-3">
+                                    <label className="label" htmlFor="name">frais en XAF : </label>
+                                    <input type="tel" name="phone" className="form-control input-buy" placeholder="Telephone"
                                         disabled value={Math.floor(state.amount*0.1*550)} />
                                 </div>
-                                <div class="form-group mb-3">
-                                    <label class="label" for="name">Numero Telephone</label>
-                                    <input type="tel" name="phone" class="form-control input-buy" placeholder="Telephone"
+                                <div className="form-group mb-3">
+                                    <label className="label" htmlFor="name">Numero Telephone</label>
+                                    <input type="tel" name="phone" className="form-control input-buy" placeholder="Telephone"
                                     required onChange={(e)=>handleChange(e.target)}/>
                                 </div>
-                                <div class="form-group my-3">
-                                    <label class="label" for="name">Nom du Compte</label>
-                                    <input type="text"  name="names" class="form-control input-buy" placeholder="Nom et Prénom" required onChange={(e)=>handleChange(e.target)}/>
+                                <div className="form-group my-3">
+                                    <label className="label" htmlFor="name">Nom du Compte</label>
+                                    <input type="text"  name="names" className="form-control input-buy" placeholder="Nom et Prénom" required onChange={(e)=>handleChange(e.target)}/>
                                 </div>
                                 <div className="wthree-text">
                                     <label className="anim">
                                         <input type="checkbox" className="checkbox" required />
-                                        <span>I Agree To The Terms & Conditions</span>
+                                        <span> I Agree To The Terms & Conditions</span>
                                     </label>
                                     <div className="clear"> </div>
                                 </div>
                                 <button className='btn-buy'>
-                                    { loader? <reactLoading type="spin" color="#ffffff" width="45px" height="45px" /> 
+                                    { loader? <ReactLoading type="spin" color="#ffffff" width="45px" height="45px" /> 
                                             : 'BUY'}
                                 </button>
-                            <p>Don't have an Account? <a href="#"> Login Now!</a></p>
+                            {/* <p>Don't have an Account? <a href="#"> Login Now!</a></p> */}
                         </div>
                         </div>
                 </div>
--- src/components/sell/Sell.jsx
+++ src/components/sell/Sell.jsx
@@ -1,19 +1,31 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import ReactLoading from 'react-loading'
 
 import './sell.css'
+const PaymentWays={
+    p1: [{lab: 'Carte Visa UBA cameroun', val: 'CB'}, {lab: 'MTN Mobile money', val: 'Mtn'}, {lab: 'Orange Money Cameroun', val: 'OM'}, {lab: 'MTN MOMO FLOAT CAMEROUN', val: 'MTNF'}, {lab: 'Carte Visa Crypto SuperKap', val: 'CBSK'}],
+    p2: [{lab: 'MOMO international', val: 'MOMO international'}, {lab: 'Carte Visa Crypto SuperKap', val: 'CBSK'}],
+    // p3: [{lab: 'Express Union', val: 'EU'}, {lab: 'Money gram', val: 'MG'}, {lab: 'Western Union', val: 'WU'}],
+    p4: [{lab: 'Cinet Pay', val: 'Cinet Pay '}, {lab: 'Carte Visa Crypto SuperKap', val: 'CBSK'}],
+    p5: [{lab: 'Cinet Pay', val: 'Cinet Pay'}, {lab: 'Carte Visa Crypto SuperKap', val: 'CBSK'}],
+    p6: [{lab: 'Carte Visa Crypto SuperKap', val: 'CBSK'}],
+
+}
 
 function Sell() {
 
-    const [state, setState]=useState({amount: "", fees: "", crypto: "Bitcoin", payment: "CB", wallet: "", numAccount: "", names: "", phone: ""})
+    const [state, setState]=useState({amount: "", fees: "", crypto: "Bitcoin", country: 'CAMEROUN-p1', payment: "CB", wallet: "", numAccount: "", names: "", phone: ""})
     const [transaction, setTransaction]=useState(null)
+    const [loader, setLoader] = useState(false);
 
     const handleChange=(target)=>{
         setState({...state, [target.name]: target.value})
     }
     const handleSubmit=(e)=>{
-        console.log(state)
         e.preventDefault()
+        setLoader(true)
+        // console.log("les data a envoyer ", state)
         fetch("https://admin-superkap.herokuapp.com/transactions.json", {
             "method": "POST",
             "headers": {
@@ -23,6 +35,7 @@ function Sell() {
                 "montant": state.amount,
                 "frais": state.amount*0.1,
                 "crypto": state.crypto,
+                "country": state.country.split('-')[0],
                 "mode_paiement": state.payment,
                 "numAccount": state.numAccount,
                 "adresse_crypto": state.wallet,
@@ -32,13 +45,20 @@ function Sell() {
             })
         })
         .then(response => response.json()).then(data=>{
+            setLoader(false);
             console.log("the data ", data)
             setTransaction(data)
         })
         .catch(err => {
+            setLoader(false);
             console.error("an error are occur ", err);
         })
         return false
+    }
+    const setPaymentsWays=()=>{
+        let way=state.country.split('-')[1]
+        console.log("the way ", way)
+        return PaymentWays[way].map((way, i)=><option key={`way-${i}`} value={way.val}>{way.lab}</option>)
     }
 
     console.log(state)
@@ -63,20 +83,29 @@ function Sell() {
                                 <div className="form-group my-4">
                                     <label className='label' for="select">Choix de la crypto</label>
                                     <select class="form-select" name="crypto" aria-label="Default select example" onChange={(e)=>handleChange(e.target)}>
-                                        <option value="Bitcoin" selected>Bitcoin</option>
+                                        <option value="Bitcoin" >Bitcoin</option>
                                         <option value="Usdt">USDT (TRC20)</option>
-                                        <option value="Ep">Euro payeer</option>
-                                        <option value="Theter">Theter </option>
+                                        <option value="Euro PAYEER">Euro payeer</option>
+                                        <option value="Litecoin">Litecoin </option>
+                                        <option value="Solana">Solana </option>
+                                        <option value="Autre">Autre... Contacter support </option>
                                     </select>
                                 </div>
                                 <div className="form-group my-4">
-                                    <label className='label' for="select">Mode de reception</label>
-                                    <select class="form-select" name="payment" aria-label="Default select example" onChange={(e)=>handleChange(e.target)}>
-                                        <option value="CB" selected>Carte Visa UBA cameroun</option>
-                                        <option value="Mtn">Mtn Mobile Money</option>
-                                        <option value="OM">Orange Money Cameroun</option>
-                                        <option value="MTNF">MTN MOMO FLOAT CAMEROUN</option>
-                                        <option value="CBSK">Carte Visa Crypto SuperKap</option>  
+                                    <label className='label' htmlFor="select">Choix du pays</label>
+                                    <select className="form-select" name="country" aria-label="Default select example" onChange={(e)=>handleChange(e.target)}>
+                                        <option value="CAMEROUN-p1" >CAMEROUN</option>
+                                        <option value="GABON/CONGO-p2">GABON/CONGO brazza </option>
+                                        {/* <option value="TCHAD/CENTRAFRIQUE-p3">TCHAD/CENTRAFRIQUE</option> */}
+                                        <option value="RDC-p4">RD CONGO</option>
+                                        <option value="UMAO-p5">CTE D'IVOIRE/MALI/SENEGAL/BENIN/BURKINA FASO/GUINE CONAKRY</option>
+                                        <option value="Autres-p6">Autre Pays</option>
+                                    </select>
+                                </div>
+                                <div className="form-group my-4">
+                                    <label className='label' htmlFor="select">Mode de paiement</label>
+                                    <select className="form-select" name="payment" aria-label="Default select example" onChange={(e)=>handleChange(e.target)}>
+                                        {setPaymentsWays()}
                                     </select>
                                 </div>
                                 <div class="form-group mb-3">
@@ -150,8 +179,11 @@ function Sell() {
                                     </label>
                                     <div className="clear"> </div>
                                 </div>
-                                <button className='btn-sell'>sell</button>
-                            <p>Don't have an Account? <a href="#"> Login Now!</a></p>
+                                <button className='btn-sell'>
+                                    { loader? <ReactLoading type="spin" color="#ffffff" width="45px" height="45px" /> 
+                                                : 'Sell'}
+                                </button>
+                            {/* <p>Don't have an Account? <a href="#"> Login Now!</a></p> */}
                         </div>
                         </div>
                 </div>
