c23791a39d20e8e680c7721f8a73a17f073bc7f2
Folley <aristotepascaldjounda@gmail.com>
2022-02-24 17:01:46 +0100

    set correct bussiness

--- src/components/formulaires/views/formulaireBusiness/FormulaireC.jsx
+++ src/components/formulaires/views/formulaireBusiness/FormulaireC.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom'
 import TextField from '@mui/material/TextField';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import { connect } from 'react-redux';
 
 import './formulairec.css'
 import bitcoin from './bitcoin.png'
@@ -12,22 +14,67 @@ import payeer from './payeer.png'
 import usdt from './usdt.png'
 import carte from './carte.png'
 import mobile from './mobile.png'
+import tof3 from '../../assets/tof3.jpg'
 
-function FormulaireC() {
-    
-    const [value, setValue] = React.useState('female');
+function FormulaireC({User, match }) {
+    const history =  useHistory();
+    if(!User.nom) {
+        history.push('/login')
+    }
+	const [payment, setPayment] = React.useState({});
+	const [value, setValue] = React.useState('female');
+	const [item, setItem] = React.useState({})
+	React.useEffect(() => {
+		fetch(`https://superkap-admin.herokuapp.com/articles/${match.params.id[1]}.json`)
+			.then(response => response.json()).then(data => {
+				console.log("the article ", data)
+				setItem(data)
+			})
+	}, [])
 
-    const handleChange = (event) => {
-      setValue(event.target.value);
-    };
+	const handleChange = (event) => {
+		setPayment(event.target.value);
+	}
+	const command = () => {
+		if (!payeer) alert("vous devez choisir um mode de payement")
+		else {
+			const body={
+				user_id: User.id,
+				article_id: match.params.id[1],
+				payment: payment
+			}
+			fetch("https://superkap-admin.herokuapp.com/commandes.json", {
+				"method": "POST",
+				"headers": {
+					"Content-Type": "application/json"
+				},
+				"body": JSON.stringify(body)
+			})
+				.then(response => {
+					if(response.status!==200) alert("echec de la commande")
+					console.log(response);
+				})
+				.catch(err => {
+					alert("echec de la commande")
+				});
+		}
+	}
 
-  return (
-    <>
-      <h1 className='mt-5 pt-5 text-center' style={{color:'#282c3f'}}>Commander Vos Articles</h1>
-      <div className="formulairec container-fluid mt-5 d-md-flex justify-content-around">
-          <div className="command-left col-12 col-md-8">
-              <h5 className='px-3 mt-3'>Details de la facturation</h5>
-              <form>
+	console.log("the item ", item, payment)
+
+	return (
+		<>
+			<h1 className='mt-5 pt-5 text-center' style={{ color: '#282c3f' }}>Commander Vos Articles</h1>
+			<div className="formulairec container-fluid mt-5 d-md-flex justify-content-around">
+				<div className="command-left col-12 col-md-8">
+					<h4 className='px-3 mt-3'>Details de la facturation</h4>
+					<hr />
+					<div className="product-details">
+						<h2> {item.nom} : <span>{item.prix} XAF</span> </h2>
+						<img src={tof3} alt="vetement" />
+					</div>
+					<>
+						{/* <form>
                   <div className="row">
                       <div className="col-12 col-md-6 shadow p-4">
                         <div class="form-group mb-3">
@@ -61,7 +108,7 @@ function FormulaireC() {
                               <select className="form-select" name="country" aria-label="Default select example" onChange={(e)=>handleChange(e.target)}>
                                   <option value="CAMEROUN-p1" >CAMEROUN</option>
                                   <option value="GABON/CONGO-p2">GABON/CONGO brazza </option>
-                                  {/* <option value="TCHAD/CENTRAFRIQUE-p3">TCHAD/CENTRAFRIQUE</option> */}
+                                  <option value="TCHAD/CENTRAFRIQUE-p3">TCHAD/CENTRAFRIQUE</option>
                                   <option value="RDC-p4">RD CONGO</option>
                                   <option value="UMAO-p5">CTE D'IVOIRE/MALI/SENEGAL/BENIN/BURKINA FASO/GUINE CONAKRY</option>
                                   <option value="Autres-p6">Autre Pays</option>
@@ -71,8 +118,8 @@ function FormulaireC() {
                               <label className='label' htmlFor="select">Choix de la ville</label>
                               <select className="form-select" name="country" aria-label="Default select example" onChange={(e)=>handleChange(e.target)}>
                                   <option value="CAMEROUN-p1" >Centre</option>
-                                  <option value="GABON/CONGO-p2">Littoral </option>
-                                  {/* <option value="TCHAD/CENTRAFRIQUE-p3">TCHAD/CENTRAFRIQUE</option> */}
+                                  <option value="GABON/CONGO-p2">Littoral</option>
+                                  <option value="TCHAD/CENTRAFRIQUE-p3">TCHAD/CENTRAFRIQUE</option>
                                   <option value="RDC-p4">Ouest</option>
                                   <option value="UMAO-p5">Extrême Nord/Nord/Adamaoua</option>
                                   <option value="UMAO-p5">Est/Sud</option>
@@ -84,37 +131,44 @@ function FormulaireC() {
                       </div>
                   </div>
                 
-              </form>
-          </div>
-          <div className="command-right col-12 col-md-3 mb-3 pb-4 px-3">
-              <h5 className=' mt-3'>Delai de la commande</h5>
-              <hr/>
-              <h6 className='d-flex'><span style={{display:'block',width:"40%"}}>Produit</span> <span style={{display:'block',width:"60%"}}>{"maison casanova ruby"}</span></h6>
-              <hr/>
-              <h6 className='d-flex'><span style={{display:'block',width:"70%"}}>frais d'expédition</span> <span style={{display:'block',width:"28%"}}> + 1000 xaf</span></h6>
-              <hr/>
-              <h6 className='d-flex'><span style={{display:'block',width:"70%"}}>T.V.A</span> <span style={{display:'block',width:"28%"}}> 0.00 xaf</span></h6>
-              <hr/>
-              <FormControl>
-                <FormLabel id="demo-controlled-radio-buttons-group">Paiement</FormLabel>
-                <RadioGroup
-                    aria-labelledby="demo-controlled-radio-buttons-group"
-                    name="controlled-radio-buttons-group"
-                    value={value}
-                    onChange={handleChange}
-                >
-                   <p className='pa'><FormControlLabel value="Bitcoin" control={<Radio />} label="Bitcoin" /><img className='imagi' src={bitcoin} alt="bitcoin" /></p>
-                   <p className='pa'><FormControlLabel value="Usdt" control={<Radio />} label="Usdt" /><img style={{width:"80px"}} src={payeer} alt="payeer"  /></p>
-                   <p className='pa'><FormControlLabel value="Payeer" control={<Radio />} label="Payeer" /><img style={{width:"40px"}} src={usdt} alt="usdt" /></p>
-                   <p className='pa'><FormControlLabel value="Carte de credit" control={<Radio />} label="Carte de credit" /><img style={{width:"65px"}} src={carte} alt="carte" /></p>
-                   <p className='pa'><FormControlLabel value="Paiement Mobile" control={<Radio />} label="Paiement Mobile" /><img style={{width:"55px"}} src={mobile} alt="mobile" /></p>
-                </RadioGroup>
-              </FormControl>
-              <button className='btn btn-lg mt-3 bg-success w-100'>Confirmer la Commande</button>
-          </div>
-      </div>
-    </>
-  )
+              </form> */}
+					</>
+				</div>
+				<div className="command-right col-12 col-md-3 mb-3 pb-4 px-3">
+					<h5 className=' mt-3'>Details de la commande</h5>
+					<hr />
+					<h6 className='d-flex'><span style={{ display: 'block', width: "40%" }}>Produit</span> <span style={{ display: 'block', width: "60%" }}>{"maison casanova ruby"}</span></h6>
+					<hr />
+					<h6 className='d-flex'><span style={{ display: 'block', width: "70%" }}>frais d'expédition</span> <span style={{ display: 'block', width: "28%" }}> + 1000 xaf</span></h6>
+					<hr />
+					<h6 className='d-flex'><span style={{ display: 'block', width: "70%" }}>T.V.A</span> <span style={{ display: 'block', width: "28%" }}> {Math.round(item.prix * 0.1925)} xaf</span></h6>
+					<hr />
+					<h6 className='d-flex'><span style={{ display: 'block', width: "70%" }}>Total</span> <span style={{ display: 'block', width: "28%" }}> {Math.round(item.prix * 1.1925) + 1000} xaf</span></h6>
+					<hr />
+					<FormControl>
+						<FormLabel id="demo-controlled-radio-buttons-group">Paiement</FormLabel>
+						<RadioGroup
+							aria-labelledby="demo-controlled-radio-buttons-group"
+							name="controlled-radio-buttons-group"
+							value={payment}
+							onChange={(e) => handleChange(e)}
+						>
+							<p className='pa'><FormControlLabel value="Bitcoin" control={<Radio />} label="Bitcoin" /><img className='imagi' src={bitcoin} alt="bitcoin" /></p>
+							<p className='pa'><FormControlLabel value="Usdt" control={<Radio />} label="Usdt" /><img style={{ width: "40px" }} src={usdt} alt="usdt" /></p>
+							<p className='pa'><FormControlLabel value="Payeer" control={<Radio />} label="Payeer" /><img style={{ width: "80px" }} src={payeer} alt="payeer" /></p>
+							<p className='pa'><FormControlLabel value="Carte de credit" control={<Radio />} label="Carte de credit" /><img style={{ width: "65px" }} src={carte} alt="carte" /></p>
+							<p className='pa'><FormControlLabel value="Paiement Mobile" control={<Radio />} label="Paiement Mobile" /><img style={{ width: "55px" }} src={mobile} alt="mobile" /></p>
+						</RadioGroup>
+					</FormControl>
+					<button className='btn btn-lg mt-3 bg-success w-100'onClick={command}>
+						Confirmer la Commande
+					</button>
+				</div>
+			</div>
+		</>
+	)
 }
 
-export default FormulaireC;
+const mapStateToProps = state => ({ User: state.User })
+
+export default connect(mapStateToProps)(FormulaireC)
--- src/components/immo2/views/details/Details.jsx
+++ src/components/immo2/views/details/Details.jsx
@@ -16,26 +16,49 @@ import fot5 from '../../assets/fot5.jpg'
 import logo from './logo-superkap.png'
 
 function Details({ User, match }) {
-    const history =  useHistory();
-    if(!User.nom) {
+    const history = useHistory();
+    if (!User.nom) {
         history.push('/login')
     }
     const [show, setShow] = useState(false);
-    const [state, setState]=useState({})
-    useEffect(()=>{
+    const [state, setState] = useState({})
+    useEffect(() => {
         fetch(`https://superkap-admin.herokuapp.com/immobiliers/${match.params.id[1]}.json`)
-            .then(response=>response.json()).then(data=>{
+            .then(response => response.json()).then(data => {
                 console.log("the appart ", data)
                 setState(data)
             })
     }, [])
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
 
-    const handle= ()=>{
-        handleClose();
+    const handle = () => {
+        setShow(false);
         history.push('/');
+    }
+    const reserver = () => {
+        const body = {
+            user_id: User.id,
+            article_id: match.params.id[1],
+        }
+        fetch("https://superkap-admin.herokuapp.com/commandes.json", {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": JSON.stringify(body)
+        })
+            .then(response => {
+                if (response.status !== 200) alert("echec de la commande")
+                else {
+                    setShow(true)
+                }
+                console.log(response);
+            })
+            .catch(err => {
+                alert("echec de la commande")
+            });
     }
 
     console.log("the state ", match, state)
@@ -45,8 +68,8 @@ function Details({ User, match }) {
                 <h1>{state.titre}</h1>
                 <div className="info">
                     <span> <FaStar className="star" size={15} color="#00B67A" /> &ensp; <b>{state.parking && "Parking"} </b></span>
-                    <span><b> { state.nombre_douche && " Salle de bain " } </b></span>
-                    <span><b> { state.cuisine && " Cuisine " } </b></span>
+                    <span><b> {state.nombre_douche && " Salle de bain "} </b></span>
+                    <span><b> {state.cuisine && " Cuisine "} </b></span>
                 </div>
                 <div className="images">
                     <div className="poster">
@@ -54,10 +77,10 @@ function Details({ User, match }) {
                         <Caroussel />
                     </div>
                     <div className="thumbnails">
-                        <img className='img-detail' src={fot2} alt=""/>
-                        <img className='img-detail' src={fot3} alt=""/>
-                        <img className='img-detail' src={fot4} alt=""/>
-                        <img className='img-detail' src={fot5} alt=""/>
+                        <img className='img-detail' src={fot2} alt="" />
+                        <img className='img-detail' src={fot3} alt="" />
+                        <img className='img-detail' src={fot4} alt="" />
+                        <img className='img-detail' src={fot5} alt="" />
                     </div>
                 </div>
             </div>
@@ -72,23 +95,23 @@ function Details({ User, match }) {
                 </div>
             </div>
             <div className="row">
-              <div className="col">
-                  {/* <p>Ma maison est située en position stratégique. À deux pas de la gare (1 heure de train de Milan) et du port (ferry-boats et bateaux à destination de toutes les localités du lac)!</p> */}
-                  <h6 style={{fontWeight:"bold"}}>Le logement :</h6>
-                  <p>{state.detail}</p>
-              </div>
+                <div className="col">
+                    {/* <p>Ma maison est située en position stratégique. À deux pas de la gare (1 heure de train de Milan) et du port (ferry-boats et bateaux à destination de toutes les localités du lac)!</p> */}
+                    <h6 style={{ fontWeight: "bold" }}>Le logement :</h6>
+                    <p>{state.detail}</p>
+                </div>
             </div>
             <div className="row carros mt-3">
-              <div className="col col-md-6">
+                <div className="col col-md-6">
 
-              </div>
-              <div className="col col-md-6 d-block d-sm-flex pt-3">
-                  <div className="col d-none d-sm-block"></div>
-                  <div className="col">
-                    <p style={{fontSize:"12px"}}>Pour protéger votre paiement, ne transférez jamais d'argent et ne communiquez pas en dehors du site ou de l'application.</p>
-                    <button onClick={handleShow} className='btn btn-outline-secondary'>contacter l'hôte</button>
-                  </div>
-              </div>
+                </div>
+                <div className="col col-md-6 d-block d-sm-flex pt-3">
+                    <div className="col d-none d-sm-block"></div>
+                    <div className="col">
+                        <p style={{ fontSize: "12px" }}>Pour protéger votre paiement, ne transférez jamais d'argent et ne communiquez pas en dehors du site ou de l'application.</p>
+                        <button onClick={reserver} className='btn btn-outline-secondary'>contacter l'hôte</button>
+                    </div>
+                </div>
             </div>
 
             <div className='mt-5'>
@@ -101,25 +124,25 @@ function Details({ User, match }) {
                     centered
                 >
                     <Modal.Header closeButton >
-                    <Modal.Title><img style={{width:'150px'}} src={logo} /></Modal.Title>
+                        <Modal.Title><img style={{ width: '150px' }} src={logo} /></Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <h5>Felicitation vous venez de reserver un Logement chez Superkap!</h5>
                         <h5>Nous vous contacterons d'ici peut merci!</h5>
                     </Modal.Body>
                     <Modal.Footer>
-                    <Button variant="success" onClick={handle} >
-                        fermer
-                    </Button>
-                        
+                        <Button variant="success" onClick={handle} >
+                            fermer
+                        </Button>
+
                     </Modal.Footer>
                 </Modal>
-                
+
             </div>
         </div>
     )
 }
 
-const mapStateToProps=state=>({User: state.User})
+const mapStateToProps = state => ({ User: state.User })
 
 export default connect(mapStateToProps)(Details)
